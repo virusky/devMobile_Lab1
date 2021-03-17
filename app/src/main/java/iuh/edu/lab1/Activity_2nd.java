e89d9321fda192e4f329be67f216ae6ba6f6392d
Virus Kei <56038340+virusky@users.noreply.github.com>
2021-03-17 19:15:28 +0700

    17/03/2021 - Lab 1

---
+++ app/src/main/java/iuh/edu/lab1/Activity_2nd.java
@@ -0,0 +1,14 @@
+package iuh.edu.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_2nd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2nd);
+    }
+}
---
+++ app/src/main/java/iuh/edu/lab1/Activity_5th.java
@@ -0,0 +1,14 @@
+package iuh.edu.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_5th extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_5th);
+    }
+}
